5ce7294e61ea4fd1a04e73cb940801e1c0420502
zzpdavid2 <zzpdavid2L@gmail.com>
2021-09-20 13:41:06 -0400

    First push

---
+++ advCS/src/advCS/UploadingToGitHub.java
@@ -0,0 +1,9 @@
+package advCS;
+
+import java.io.*;
+
+public class UploadingToGitHub {
+    public static void main(String[] args) throws FileNotFoundException {
+    	System.out.print("Hi I am David.");
+    }
+}
