17f74ffdd6e8453ef87d7df6684c20b06c249a63
zzpDavid2 <zzpDavid2L@gmail.com>
2021-09-22 13:22:25 -0400

    Implemented war method.
    Also refined different aspects of the code.

--- advCS/src/war/War.java
+++ advCS/src/war/War.java
@@ -5,6 +5,11 @@ import java.io.*;
 
 public class War {
     public static void main(String[] args) throws FileNotFoundException {
+    	play();
+    	
+    }
+    
+    public static void play() {
     	LinkedList<Card> deck = new LinkedList<Card>();
     	
     	for(int i=1; i<14; i++) {
@@ -19,7 +24,7 @@ public class War {
     	for(int i=1; i<14; i++) {
     		deck.add(new Card(i,"spades"));
     	}
-    	
+
     	Scanner scn = new Scanner(System.in);
     	
     	System.out.println("Hi, welcome to card game: War");
@@ -30,7 +35,17 @@ public class War {
     	LinkedList<Card> P2 = new LinkedList<Card>();
     	
     	System.out.println("How many cards should each player get? (26 max)");
-    	int numCards = scn.nextInt();
+    	
+    	int numCards;
+    	
+    	try {
+    		numCards = scn.nextInt();
+    	}
+    	catch(InputMismatchException e) {
+        	System.out.println("Invalid input. Defaulted to 26.");
+        	scn.next();
+        	numCards = 26;
+    	}
     	
     	if(numCards >=26) {
     		numCards = 26;
@@ -40,6 +55,24 @@ public class War {
     		P1.add(deck.get(i));
     		P2.add(deck.get(numCards+i));
     	}
+    	
+    	boolean haveWar = true;
+    	
+    	System.out.println("Play the game with war rules? (Y/N)");
+    	String s = scn.next();
+    	
+    	if(s.equals("y") || s.equals("Y")) {
+        	System.out.println("War rules are in play.");
+        	haveWar = true;
+    	}else if(s.equals("n") || s.equals("N")) {
+        	System.out.println("War rules are deactivated, both cards are disgarded for a draw.");
+    		haveWar = false;
+    	}else {
+        	System.out.println("Invalid input. War rules are in play by default.");
+        	haveWar = true;
+    	}
+    	
+    	scn.nextLine();
     	
     	System.out.println("Cards are shuffled. Press ENTER to play!");
     	scn.nextLine();
@@ -67,7 +100,11 @@ public class War {
     			P2.addLast(C1);
     			P2.addLast(C2);
     		}else {
-    			System.out.println("Draw! Both cards are disgarded.");
+    			if(haveWar) {
+    				cardWar(P1,P2,C1,C2);
+    			}else{
+    				System.out.println("Draw! Both cards are disgarded.");
+    			}
     		}
     		
     		N1 = P1.size();
@@ -87,7 +124,7 @@ public class War {
     		System.out.print(N1);
     		System.out.print(" cards remaining; Player 2 have ");
     		System.out.print(N2);
-    		System.out.print(" cards remaining.");
+    		System.out.println(" cards remaining.");
     		
     		System.out.println("Press ENTER to deal cards.");
         	scn.nextLine();
@@ -103,7 +140,6 @@ public class War {
     	}
     	
     	scn.close();
-    	
     }
     
     public static void shuffle(LinkedList<Card> ll) {
@@ -119,5 +155,66 @@ public class War {
     		
     	}
     	
+    }
+    
+    public static void cardWar(LinkedList p1, LinkedList p2, Card o1, Card o2) {
+    	System.out.println("War! Each player draws 4 cards, the last card decides the winner.");
+    	
+    	LinkedList<Card> warCards = new LinkedList<Card>();
+    	
+    	warCards.add(o1);
+    	warCards.add(o2);
+    	
+    	boolean end = false;
+
+    	while(!end) {
+        	System.out.println("P1 draws the following cards to war:");
+    		for(int i=0; i<3; i++) {
+    			Card polled = (Card) p1.pollFirst();
+    			warCards.add(polled);
+    			System.out.println(polled);
+    		}
+    		
+        	System.out.println("P2 draws the following cards to war:");
+    		for(int i=0; i<3; i++) {
+    			Card polled = (Card) p2.pollFirst();
+    			warCards.add(polled);
+    			System.out.println(polled);
+    		}
+    		
+    		Card C1 = (Card) p1.pollFirst();
+			warCards.add(C1);
+    		Card C2 = (Card) p2.pollFirst();
+			warCards.add(C2);
+    		
+    		System.out.print("Player 1 plays the ");
+    		System.out.println(C1);
+    		System.out.print("Player 2 plays the ");
+    		System.out.println(C2);
+    		
+    		if(C1.getNumber()>C2.getNumber()) {
+    			System.out.println("Player 1 wins the war!");
+    			System.out.println("Player 1 gets all the following cards from the war:");
+    			for(Card c : warCards) {
+    				System.out.println(c);
+    				p1.add(c);
+    			}
+    			end = true;
+    			break;
+    			
+    		}else if(C2.getNumber()>C1.getNumber()) {
+    			System.out.println("Player 2 wins the war!");
+    			System.out.println("Player 2 gets all the following cards from the war:");
+    			for(Card c : warCards) {
+    				System.out.println(c);
+    				p2.add(c);
+    			}
+    			end = true;
+    			break;
+    			
+    		}else {
+    			System.out.println("Draw! The war goes on.");
+    		}
+    	}
     }
 }
